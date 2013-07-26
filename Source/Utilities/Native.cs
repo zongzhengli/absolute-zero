fa82c1a3ba0482f1daf6bdf2cfb4c68e5e84a6ae
Zong Zheng Li <contact@zongzhengli.com>
2013-07-26 13:05:26 -0400

    Document Native class
    New: documentation for all methods.

--- Source/Utilities/Native.cs
+++ Source/Utilities/Native.cs
@@ -2,12 +2,26 @@
 using System.Runtime.InteropServices;
 
 namespace AbsoluteZero {
+
+    /// <summary>
+    /// Provides Windows native methods. 
+    /// </summary>
     class Native {
         public const Int32 SW_HIDE = 0;
 
+        /// <summary>
+        /// Returns a handle to the command-line console. 
+        /// </summary>
+        /// <returns>A handle to the command-line console</returns>
         [DllImport("kernel32.dll")]
         public static extern IntPtr GetConsoleWindow();
 
+        /// <summary>
+        /// Sets the specified window's show state.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window.</param>
+        /// <param name="nCmdShow">A flag indicating how the window is to be shown.</param>
+        /// <returns>Whether the window was previously visible.</returns>
         [DllImport("user32.dll")]
         public static extern Boolean ShowWindow(IntPtr hWnd, Int32 nCmdShow);
     }
