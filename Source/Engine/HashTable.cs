0de658c2262d299101309dae847d9c55cc3281ee
Zong Zheng Li <contact@zongzhengli.com>
2013-08-11 01:53:32 -0400

    Document HashTable class
    New: documentation for HashTable fields and methods

--- Source/Engine/HashTable.cs
+++ Source/Engine/HashTable.cs
@@ -2,14 +2,41 @@
 using System.Collections.Generic;
 
 namespace AbsoluteZero {
+
+    /// <summary>
+    /// The transposition table component of the Absolute Zero chess engine. 
+    /// </summary>
     partial class Zero {
+
+        /// <summary>
+        /// Defines a transposition hash table. 
+        /// </summary>
         private class HashTable {
+
+            /// <summary>
+            /// The collection of hash entries. 
+            /// </summary>
             private HashEntry[] entries;
+
+            /// <summary>
+            /// The value used to index into the array storing the HashEntrys.
+            /// </summary>
             private UInt64 indexer;
 
+            /// <summary>
+            /// The number of entries stored in the HashTable. 
+            /// </summary>
             public Int32 Count = 0;
+
+            /// <summary>
+            /// The number of entries that can be stored in the HashTable. 
+            /// </summary>
             public Int32 Capacity;
 
+            /// <summary>
+            /// Constructs a HashTable of the given size in megabytes. 
+            /// </summary>
+            /// <param name="megabytes">The size of the new HashTable in megabytes.</param>
             public HashTable(Int32 megabytes) {
                 Capacity = (megabytes << 20) / HashEntry.Size;
                 indexer = (UInt64)Capacity;
