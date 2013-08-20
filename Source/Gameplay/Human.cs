51ee1f61433a0de769ddd51fff8ec6692f8be8a0
Zong Zheng Li <contact@zongzhengli.com>
2013-08-20 18:13:05 -0400

    Document Human class
    New: documentation for class and all methods and fields

--- Source/Gameplay/Human.cs
+++ Source/Gameplay/Human.cs
@@ -4,16 +4,53 @@ using System.Threading;
 using System.Windows.Forms;
 
 namespace AbsoluteZero {
+
+    /// <summary>
+    /// Defines a human player in the chess game. 
+    /// </summary>
     class Human : IPlayer {
+
+        /// <summary>
+        /// The brush used to paint the piece selection background. 
+        /// </summary>
         static readonly SolidBrush SelectionBrush = new SolidBrush(Color.White);
 
+        /// <summary>
+        /// The ManualResetEvent that blocks the running thread to wait for the 
+        /// player to move. 
+        /// </summary>
         private ManualResetEvent WaitForMove = new ManualResetEvent(false);
+
+        /// <summary>
+        /// Whether the player is current deciding on a move. 
+        /// </summary>
         private Boolean IsMoving = false;
+
+        /// <summary>
+        /// The initial square for the player's move.
+        /// </summary>
         private Int32 InitialSquare;
+
+        /// <summary>
+        /// The final square for the player's move. 
+        /// </summary>
         private Int32 FinalSquare;
+
+        /// <summary>
+        /// The current position the player is moving on.
+        /// </summary>
         private Position CurrentPosition;
+
+        /// <summary>
+        /// The colour of the player. 
+        /// </summary>
         private Int32 currentColour;
 
+        /// <summary>
+        /// Returns the player's move for the given position. 
+        /// </summary>
+        /// <param name="position">The position to make a move on.</param>
+        /// <returns>The player's move.</returns>
         public Int32 GetMove(Position position) {
             CurrentPosition = position;
             currentColour = position.SideToMove;
@@ -25,23 +62,44 @@ namespace AbsoluteZero {
             return CreateMove(position, InitialSquare, FinalSquare);
         }
 
+        /// <summary>
+        /// Return whether the player is willing to accept a draw offer. 
+        /// </summary>
+        /// <returns></returns>
         public Boolean AcceptDraw() {
             return false;
         }
-
+        
+        /// <summary>
+        /// Stops the wait for the player's move if applicable. 
+        /// </summary>
         public void Stop() {
             Reset();
         }
 
+        /// <summary>
+        /// Resets the player's fields. 
+        /// </summary>
         public void Reset() {
             IsMoving = false;
             WaitForMove.Reset();
         }
 
+        /// <summary>
+        /// Returns the name of the player. 
+        /// </summary>
+        /// <returns></returns>
         public String GetName() {
             return "Human";
         }
 
+        /// <summary>
+        /// Returns the move specified by the given information.
+        /// </summary>
+        /// <param name="position">The position the move is to be played on.</param>
+        /// <param name="from">The initial square of the move.</param>
+        /// <param name="to">The final square of the move.</param>
+        /// <returns>The move specified by the given information.</returns>
         private Int32 CreateMove(Position position, Int32 from, Int32 to) {
             foreach (Int32 move in position.LegalMoves())
                 if (from == Move.From(move) && to == Move.To(move)) {
@@ -66,6 +124,10 @@ namespace AbsoluteZero {
             return Move.Invalid;
         }
 
+        /// <summary>
+        /// Handles a mouse up event.
+        /// </summary>
+        /// <param name="e">The mouse event.</param>
         public void MouseUpEvent(MouseEventArgs e) {
             if (!IsMoving)
                 return;
@@ -83,6 +145,10 @@ namespace AbsoluteZero {
             }
         }
 
+        /// <summary>
+        /// Draws the player's graphical elements. 
+        /// </summary>
+        /// <param name="g">The drawing surface.</param>
         public void Draw(Graphics g) {
             if (IsMoving && InitialSquare != Position.InvalidSquare)
                 VisualPosition.FillSquare(g, SelectionBrush, InitialSquare);
