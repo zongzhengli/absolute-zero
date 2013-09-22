ccc6da218f64ef72f4c491bc209daf0708b65f62
Zong Zheng Li <contact@zongzhengli.com>
2013-09-21 21:45:32 -0400

    Document Move class
    New: documentation for all methods.
    Change: improve documentation for class and fields.

--- Source/Framework/Move.cs
+++ Source/Framework/Move.cs
@@ -4,32 +4,32 @@ using PieceClass = AbsoluteZero.Piece;
 namespace AbsoluteZero {
 
     /// <summary>
-    /// Provides move methods. 
+    /// Provides move encoding and decoding methods. 
     /// </summary>
     static class Move {
 
         /// <summary>
-        /// The invalid move.
+        /// The value representing an invalid move.
         /// </summary>
         public const Int32 Invalid = 0;
 
         /// <summary>
-        /// The amount the to square is shifted in the move. 
+        /// The amount the to square is shifted when encoding the move. 
         /// </summary>
         private const Int32 ToShift = 6;
 
         /// <summary>
-        /// The amount the piece is shifted in the move.
+        /// The amount the moving piece is shifted when encoding the move.
         /// </summary>
         private const Int32 PieceShift = ToShift + 6;
 
         /// <summary>
-        /// The amount the captured piece is shifted in the move.
+        /// The amount the captured piece is shifted when encoding the move.
         /// </summary>
         private const Int32 CaptureShift = PieceShift + 4;
 
         /// <summary>
-        /// The amount the special piece is shifted in the move.
+        /// The amount the special piece is shifted when encoding the move.
         /// </summary>
         private const Int32 SpecialShift = CaptureShift + 4;
 
@@ -74,7 +74,7 @@ namespace AbsoluteZero {
         private const Int32 PawnSpecialShifted = PieceClass.Pawn << SpecialShift;
 
         /// <summary>
-        /// The value of the queen special (queen promotion) exactly as it is 
+        /// The value of the queen special (promotion to queen) exactly as it is 
         /// represented in the move. 
         /// </summary>
         private const Int32 QueenSpecialShifted = PieceClass.Queen << SpecialShift;
@@ -90,14 +90,36 @@ namespace AbsoluteZero {
         /// </summary>
         private const Int32 PawnPieceShifted = PieceClass.Pawn << PieceShift;
 
+        /// <summary>
+        /// Returns the pawn's move bitboard for the given square as the given 
+        /// colour. 
+        /// </summary>
+        /// <param name="square">The square the pawn is on.</param>
+        /// <param name="colour">The colour of the pawn.</param>
+        /// <returns>The pawn's move bitboard.</returns>
         public static UInt64 Pawn(Int32 square, Int32 colour) {
             return 1UL << (square + 16 * colour - 8);
         }
 
+        /// <summary>
+        /// Returns a move encoded from the given parameters. 
+        /// </summary>
+        /// <param name="position">The position the move is to be played on.</param>
+        /// <param name="from">The from square of the move.</param>
+        /// <param name="to">The to square of the move.</param>
+        /// <param name="special">The special piece of the move.</param>
+        /// <returns>A move encoded from the given parameters.</returns>
         public static Int32 Create(Position position, Int32 from, Int32 to, Int32 special = PieceClass.Empty) {
             return from | (to << ToShift) | (position.Square[from] << PieceShift) | (position.Square[to] << CaptureShift) | (special << SpecialShift);
         }
 
+        /// <summary>
+        /// Returns a move to be played on the given position that has the given 
+        /// representation in coordinate notation. 
+        /// </summary>
+        /// <param name="position">The position the move is to be played on.</param>
+        /// <param name="name">The representation of the move in coordinate notation.</param>
+        /// <returns>A move that has the given representation in coordinate notation.</returns>
         public static Int32 Create(Position position, String name) {
             foreach (Int32 move in position.LegalMoves())
                 if (name == Identify.Move(move))
@@ -105,34 +127,74 @@ namespace AbsoluteZero {
             return Invalid;
         }
 
+        /// <summary>
+        /// Returns the from square of the given move. 
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>The from square of the given move.</returns>
         public static Int32 From(Int32 move) {
             return move & SquareMask;
         }
 
+        /// <summary>
+        /// Returns the to square of the given move.
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>The to square of the given move.</returns>
         public static Int32 To(Int32 move) {
             return (move >> ToShift) & SquareMask;
         }
 
+        /// <summary>
+        /// Returns the moving piece of the given move.
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>The moving piece of the given move.</returns>
         public static Int32 Piece(Int32 move) {
             return (move >> PieceShift) & PieceMask;
         }
 
+        /// <summary>
+        /// Returns the captured piece of the given move.
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>The captured piece of the given move.</returns>
         public static Int32 Capture(Int32 move) {
             return (move >> CaptureShift) & PieceMask;
         }
-
+        
+        /// <summary>
+        /// Returns the special piece of the given move. 
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>The special piece of the given move.</returns>
         public static Int32 Special(Int32 move) {
             return move >> SpecialShift;
         }
 
+        /// <summary>
+        /// Returns whether the given move captures an opposing piece.
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>Whether the given move captures an opposing piece.</returns>
         public static Boolean IsCapture(Int32 move) {
             return (move & TypeCaptureShifted) != EmptyCaptureShifted;
         }
 
+        /// <summary>
+        /// Returns whether the given move is a castling move. 
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>Whether the given move is a castling move.</returns>
         public static Boolean IsCastle(Int32 move) {
             return (move & TypeSpecialShifted) == KingSpecialShifted;
         }
 
+        /// <summary>
+        /// Returns whether the given move promotes a pawn.
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>Whether the given mode promotes a pawn.</returns>
         public static Boolean IsPromotion(Int32 move) {
             if ((move & TypePieceShifted) != PawnPieceShifted)
                 return false;
@@ -140,14 +202,29 @@ namespace AbsoluteZero {
             return (to - 8) * (to - 55) > 0;
         }
 
+        /// <summary>
+        /// Returns whether the given move is en passant. 
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>Whether the given mode is en passant.</returns>
         public static Boolean IsEnPassant(Int32 move) {
             return (move & TypeSpecialShifted) == PawnSpecialShifted;
         }
 
+        /// <summary>
+        /// Returns whether the given move is a pawn advance.
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>Whether the given move is a pawn advance.</returns>
         public static Boolean IsPawnAdvance(Int32 move) {
             return (move & TypePieceShifted) == PawnPieceShifted;
         }
 
+        /// <summary>
+        /// Returns whether the given move promotes a pawn to a queen. 
+        /// </summary>
+        /// <param name="move">The move to decode.</param>
+        /// <returns>Whether the given mode promotes a pawn to a queen.</returns>
         public static Boolean IsQueenPromotion(Int32 move) {
             return (move & TypeSpecialShifted) == QueenSpecialShifted;
         }
