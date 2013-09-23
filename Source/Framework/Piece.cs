348758461f694a4bde62526157c4c909e412cae3
Zong Zheng Li <contact@zongzhengli.com>
2013-09-23 19:17:36 -0400

    Document Piece class
    New: documentation for all piece constants.
    Change: better wording and formatting for existing documentation.

--- Source/Framework/Piece.cs
+++ Source/Framework/Piece.cs
@@ -3,26 +3,78 @@
 namespace AbsoluteZero {
 
     /// <summary>
-    /// Defines piece values. 
+    /// Defines pieces and masks. 
     /// </summary>
     static class Piece {
+        
+        /// <summary>
+        /// The mask for extracting the type of a piece. 
+        /// </summary>
+        public const Int32 Type = 14;                                                // 1110
 
-        public const Int32 Type = 14;                   // 1110
-        public const Int32 Colour = Black;              // 0001
+        /// <summary>
+        /// The mask for extracting the colour of a piece. 
+        /// </summary>
+        public const Int32 Colour = Black;                                           // 0001
 
-        public const Int32 White = 0;                   // 0000
-        public const Int32 Black = 1;                   // 0001
+        /// <summary>
+        /// Represents the colour white. 
+        /// </summary>
+        public const Int32 White = 0;                                                // 0000
 
+        /// <summary>
+        /// Represents the colour black. 
+        /// </summary>
+        public const Int32 Black = 1;                                                // 0001
+
+        /// <summary>
+        /// The smallest value used to represent a piece. 
+        /// </summary>
         public const Int32 Min = White | Pawn;
+
+        /// <summary>
+        /// The largest value used to represent a piece. 
+        /// </summary>
         public const Int32 Max = Black | King;
 
-        public const Int32 Empty = 0;                   // 0000
-        public const Int32 Pawn = 2;                    // 0010
-        public const Int32 Knight = 4;                  // 0100
-        public const Int32 Bishop = 6;                  // 0110
-        public const Int32 Rook = 8;                    // 1000
-        public const Int32 Queen = 10;                  // 1010
-        public const Int32 King = 12;                   // 1100
-        public const Int32 All = 14;                    // 1110
+        /// <summary>
+        /// Represents the empty, or non-existent, piece. 
+        /// </summary>
+        public const Int32 Empty = 0;                                                // 0000
+
+        /// <summary>
+        /// Represents the pawn. 
+        /// </summary>
+        public const Int32 Pawn = 2;                                                 // 0010
+
+        /// <summary>
+        /// Represents the knight. 
+        /// </summary>
+        public const Int32 Knight = 4;                                               // 0100
+
+        /// <summary>
+        /// Represents the bishop. 
+        /// </summary>
+        public const Int32 Bishop = 6;                                               // 0110
+
+        /// <summary>
+        /// Represents the rook. 
+        /// </summary>
+        public const Int32 Rook = 8;                                                 // 1000
+
+        /// <summary>
+        /// Represents the queen. 
+        /// </summary>
+        public const Int32 Queen = 10;                                               // 1010
+
+        /// <summary>
+        /// Represents the king. 
+        /// </summary>
+        public const Int32 King = 12;                                                // 1100
+
+        /// <summary>
+        /// Represents all the pieces. 
+        /// </summary>
+        public const Int32 All = 14;                                                 // 1110
     }
 }
