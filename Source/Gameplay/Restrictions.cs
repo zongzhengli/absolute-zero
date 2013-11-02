e8bed1ad91cb456019cc3c0dce6b1530261395b4
Zong Zheng Li <contact@zongzhengli.com>
2013-11-02 15:53:25 -0400

    Document Restrictions class
    New: documentation for Restrictions class, methods, and fields.

--- Source/Gameplay/Restrictions.cs
+++ Source/Gameplay/Restrictions.cs
@@ -1,22 +1,65 @@
 ﻿using System;
 
 namespace AbsoluteZero {
+
+    /// <summary>
+    /// Specifies the output type.
+    /// </summary>
     public enum OutputType { Standard, Universal, None }
 
+    /// <summary>
+    /// Defines the restrictions for engines in standard games. 
+    /// </summary>
     static class Restrictions {
+
+        /// <summary>
+        /// The output type.
+        /// </summary>
         public static OutputType Output = OutputType.Standard;
+
+        /// <summary>
+        /// The maximum number of milliseconds to use when moving. 
+        /// </summary>
         public static Int32 MoveTime;
+
+        /// <summary>
+        /// The maximum depth to search to when moving.
+        /// </summary>
         public static Int32 Depth;
+
+        /// <summary>
+        /// The maximum number of nodes to search when moving. 
+        /// </summary>
         public static Int64 Nodes;
 
+        /// <summary>
+        /// Whether to use time controls. 
+        /// </summary>
         public static Boolean UseTimeControls;
+
+        /// <summary>
+        /// The time left for given side. TimeControl[c] gives the number of 
+        /// milliseconds left on the clock for colour c.
+        /// </summary>
         public static Int32[] TimeControl;
+
+        /// <summary>
+        /// The time increment for given side. TimeIncrement[c] gives the number of 
+        /// milliseconds incremented for colour c after every move.
+        /// </summary>
         public static Int32[] TimeIncrement;
 
+        /// <summary>
+        /// Initializes default values. 
+        /// </summary>
         static Restrictions() {
             Reset();
         }
 
+        /// <summary>
+        /// Resets the restrictions to the default values, with the exception of 
+        /// output type.
+        /// </summary>
         public static void Reset() {
             UseTimeControls = false;
             TimeControl = new Int32[2];
